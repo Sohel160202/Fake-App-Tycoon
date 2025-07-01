57e3e0426741c8ff36177bc4a1ae5d6a51e4cd53
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-02 03:23:43 +0600

    Update GameStore.js

--- src/store/GameStore.js
+++ src/store/GameStore.js
@@ -1,1 +1,38 @@
-// Zustand game store
+// src/store/GameStore.js
+import { create } from 'zustand';
+
+const useGameStore = create((set) => ({
+  // Core App Info
+  appName: '',
+
+  // Simulation Stats
+  funding: 0,
+  hype: 0,
+  reputation: 5,
+  bugs: 0,
+  users: 0,
+
+  // Current Phase
+  currentPhase: 'pitch',
+
+  // Actions
+  setAppName: (name) => set({ appName: name }),
+
+  updateStat: (stat, amount) =>
+    set((state) => ({ [stat]: Math.max(0, (state[stat] || 0) + amount) })),
+
+  setPhase: (phase) => set({ currentPhase: phase }),
+
+  resetGame: () =>
+    set({
+      appName: '',
+      funding: 0,
+      hype: 0,
+      reputation: 5,
+      bugs: 0,
+      users: 0,
+      currentPhase: 'pitch'
+    })
+}));
+
+export default useGameStore;

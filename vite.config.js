807245a40f6aee1b98ef691e80dd3bc7754ef31e
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-02 04:20:51 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  root: '.', // ensure it looks in the root
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 });
